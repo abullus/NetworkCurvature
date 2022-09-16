acabd8f146f5d198d38564993312fe1b6e15fd4a
abullus <alfiebullus@live.com>
2022-09-17 00:03:55 +0100

    Work organised and completed to working standard

---
+++ demos/EdgeBasedMeasuresComparison.jl
@@ -0,0 +1,59 @@
+using Graphs, DataFrames
+using Base.Threads
+
+include("../src/NetworkCurvature.jl")
+using .NetworkCurvature
+
+function get_embeddedness(G, e)
+    Adj_u = neighbors(G, e.src)
+    Adj_v = neighbors(G, e.dst)
+    return length(intersect(Adj_u, Adj_v))
+end
+
+function export_data(filename)
+    G = load_graph_from_edgelist(filename)
+
+    weighted = typeof(G) !== SimpleGraph{Int}
+
+    curvature_dist = get_edge_curvature_distribution(G)
+
+    source = Int[]
+    destination = Int[]
+    weight = Float64[]
+    curvature = Float64[]
+    embeddedness = Int[]
+
+    for e in edges(G)
+        push!(source, e.src)
+        push!(destination, e.dst)
+        if weighted
+            push!(weight, e.weight)
+        end
+        push!(curvature, curvature_dist[e])
+        push!(embeddedness, get_embeddedness(G, e))
+    end
+
+    df = DataFrame()
+    df.Source = source
+    df.Destination = destination
+    if weighted
+        df.Weighted = weight
+    end
+    df.Edge_Curvature = curvature
+    df.Embeddedness = embeddedness
+
+    export_data_frame_to_csv(df, filename*"-EdgeData.csv")
+end
+
+#=
+files =  ["bio-WormNet-v3.edges"]
+for filename in files
+    print("Generating for dataset: ", filename)
+    export_data(filename)
+    println(" done!")
+end
+=#
+
+#export_data("demos/bio-WormNet-v3.edges")
+export_data("demos/cond-mat-2003.txt")
+export_data("demos/Astrophysics.txt")
---
+++ demos/NodeBasedMeasuresComparison.jl
@@ -0,0 +1,58 @@
+using Graphs, DataFrames
+import Graphs.Parallel
+
+include("../src/NetworkCurvature.jl")
+using .NetworkCurvature
+
+# Computations are rounded to save storage space
+round_vec(vec, sigdigits=4) = [round.(x, sigdigits=sigdigits) for x in vec]
+
+function export_data(filename)
+    G = loadedgelist_weighted(filename)
+
+    # Computationally intensive calculations are done here so that progress can be tracked.
+    # The final calculations were too intensive for most networks.
+    println("Node curvature...")
+    curvature_dist = get_node_curvature_distribution(G)
+    println("Betweenness...")
+    betweenness = round_vec(Parallel.betweenness_centrality(G, min(nv(G), 100)))
+    println("Pagerank...")
+    pagerank = round_vec(Parallel.pagerank(G))
+    println("LCC...")
+    lcc = round_vec(local_clustering_coefficient(G))
+    println("Eigenvector centrality...")
+    eig_cen = round_vec(eigenvector_centrality(G))
+    #=
+    println("Closeness centrality...")
+    closeness = round_vec(closeness_centrality(G))
+    println("Katz centrality...")
+    katz = round_vec(katz_centrality(G))
+    =#
+
+    df = DataFrame()
+    df.Node = [v for v in vertices(G)]
+    df.Degree = degree(G)
+    df.Node_Curvature = [curvature_dist[v] for v in vertices(G)]
+    df.Triangles = triangles(G)
+    df.Betweenness_Centrality = betweenness
+    df.Pagerank = pagerank
+    df.Local_Clustering_Coefficient = lcc
+    df.Eigenvector_Centrality = eig_cen
+    # df.Closeness_Centrality = closeness
+    # df.Katz_Centrality = katz
+        
+
+    export_data_frame_to_csv(df, filename*"-NodeData.csv")
+end
+
+#=
+files = ["rec-eachmovie-clean"]#["inf-USAir97" , "eco-florida", "cond-mat-2003", "bio-WormNet-v3",  "rec-eachmovie"]
+for filename in files
+    print("Generating for dataset: ", filename)
+    export_data(filename)
+    println(" done!")
+end
+=#
+
+#export_data("demos/bio-WormNet-v3.edges")
+export_data("demos/cond-mat-2003.txt")
