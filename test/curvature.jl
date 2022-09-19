53951227f94116cdf5e0628cd58942900bc8e0ad
abullus <alfiebullus@live.com>
2022-09-20 00:23:47 +0100

    Added node curvature test

--- test/curvature.jl
+++ test/curvature.jl
@@ -1,13 +1,15 @@
 @testset "Curvature" begin
+
     G = SimpleGraph(6)
     add_edge!(G, 1, 2); add_edge!(G, 3, 4); add_edge!(G, 4, 5); add_edge!(G, 5, 6); add_edge!(G, 4, 6)
     W = SimpleWeightedGraph(7)
     add_edge!(W, 1, 2, 4); add_edge!(W, 3, 4, 9); add_edge!(W, 4, 5, 4); add_edge!(W, 3, 5, 4); 
     add_edge!(W, 5, 6, 16); add_edge!(W, 6, 7, 1); 
+
     @testset "Edge curvature" begin
         @test get_edge_curvature(G, 1, 2) == 2
 
-        @test get_edge_curvature_distribution(G) == Dict{Graphs.SimpleGraphs.SimpleEdge{Int64}, Int64}(
+        @test get_edge_curvature_distribution(G) == Dict(
             Edge(1, 2) => 2,
             Edge(3, 4) => 0,
             Edge(4, 5) => -1,
@@ -17,13 +19,19 @@
 
         @test round(get_edge_curvature(W, 3, 5), sigdigits=4) == round(-1/6, sigdigits=4)
 
-        @test get_edge_curvature_distribution(W) ==  Dict{SimpleWeightedEdge{Int64, Float64}, Float64}(
+        @test get_edge_curvature_distribution(W) ==  Dict(
             SimpleWeightedEdge(5,6,16.0) => -6.0,
             SimpleWeightedEdge(3,4,9.0) => -1.0,
             SimpleWeightedEdge(3,5,4.0) => round(-1/6, sigdigits=4),
             SimpleWeightedEdge(1,2,4.0) => 2.0,
             SimpleWeightedEdge(6,7,1.0) => 1.75,
             SimpleWeightedEdge(4,5,4.0) => round(-1/6, sigdigits=4)
+            )
+    end
+
+    @testset "Node curvature" begin
+        @test get_node_curvature_distribution(G) == Dict(
+            1 => 2.0, 2 => 2.0, 3 => 0.0, 4 => -2.0, 5 => -1.0, 6 => -1.0
             )
     end
 end
